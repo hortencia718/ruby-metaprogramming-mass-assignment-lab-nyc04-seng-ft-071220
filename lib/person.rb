436cf5fd076a84ba732e81f233e759c3470e5864
hortencia c <hortenciaecisneros@gmail.com>
2020-08-12 00:13:13 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,7 +6,8 @@ class Person
   
   def initialize(attributes=nil)
   if  attributes
-    attributes.each {| key,value|self.send(("#{key}="), value)}
+    attributes.each {|key,value|
+    self.send(("#{key}="), value)}
    end
   end 
  end
